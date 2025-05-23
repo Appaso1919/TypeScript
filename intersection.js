18d4568687f15af4363a2767226dac41152a390b
Appaso Sanjay Avaghadi <appaso1919@gmail.com>
2025-05-23 13:55:30 +0530

    Intersection

---
+++ intersection.js
@@ -0,0 +1,8 @@
+// type personTA={name:string}
+// type personTB={age:number}
+// type personTC=personTA & personTB
+var PersonDataA = { name: 'Anil sidhu' };
+var PersonDataB = { age: 30 };
+var PersonDataC = {
+    name: 'Sidhu', age: 30
+};
