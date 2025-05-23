c62e57da9e53194a8a8286945047b7d8301622ba
Appaso Sanjay Avaghadi <appaso1919@gmail.com>
2025-05-23 17:08:05 +0530

    class and object and inheritance

--- class.js
+++ class.js
@@ -1,3 +1,19 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+// Basic Class
 var Person = /** @class */ (function () {
     function Person(name, age) {
         this.name = name;
@@ -10,3 +26,35 @@ var Person = /** @class */ (function () {
 }());
 var p1 = new Person("Appaso", 22);
 p1.greet();
+//Inheritence
+var Animal = /** @class */ (function () {
+    function Animal() {
+    }
+    Animal.prototype.move = function () {
+        console.log("Animal is moving");
+    };
+    return Animal;
+}());
+var Dog = /** @class */ (function (_super) {
+    __extends(Dog, _super);
+    function Dog() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Dog.prototype.bark = function () {
+        console.log("Dog is barking");
+    };
+    return Dog;
+}(Animal));
+var d = new Dog();
+d.move(); // from Animal
+d.bark(); // from Dog
+var ConsoleLogger = /** @class */ (function () {
+    function ConsoleLogger() {
+    }
+    ConsoleLogger.prototype.log = function (message) {
+        console.log("Log:", message);
+    };
+    return ConsoleLogger;
+}());
+var logger = new ConsoleLogger();
+logger.log("Hello from interface-based class!");
--- class.ts
+++ class.ts
@@ -1,3 +1,4 @@
+// Basic Class
 class Person {
   name: string;
   age: number;
@@ -14,3 +15,34 @@ class Person {
 
 const p1 = new Person("Appaso", 22);
 p1.greet();
+
+
+//Inheritence
+class Animal {
+  move(): void {
+    console.log("Animal is moving");
+  }
+}
+class Dog extends Animal {
+  bark(): void {
+    console.log("Dog is barking");
+  }
+}
+const d = new Dog();
+d.move(); // from Animal
+d.bark(); // from Dog
+
+
+//class with interface
+interface Logger {
+  log(message: string): void;
+}
+
+class ConsoleLogger implements Logger {
+  log(message: string): void {
+    console.log("Log:", message);
+  }
+}
+
+const logger = new ConsoleLogger();
+logger.log("Hello from interface-based class!");
